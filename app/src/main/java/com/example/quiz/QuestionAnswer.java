d53a8c468e092814b9690a6a64fdfaa2a70626ca
Obaidur Rahman <obaidurrahmanpc@gmail.com>
2023-02-20 21:08:42 +0530

    Quiz App

---
+++ app/src/main/java/com/example/quiz/QuestionAnswer.java
@@ -0,0 +1,26 @@
+package com.example.quiz;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "What is the default value of int variable?",
+            "Which one is not the programming language?",
+            "Which one of the following is not a Java feature?",
+            "Which company owns the android?"
+    };
+
+    public static String choices[][] = {
+            {"0","0.0","null","not define"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Object-oriented","Use of pointers","Portable","Dynamic and Extensible"},
+            {"Google","Apple","Nokia","Samsung"}
+    };
+
+    public static String correctAnswers[] = {
+            "0",
+            "Notepad",
+            "Use of pointers",
+            "Google"
+    };
+
+}
